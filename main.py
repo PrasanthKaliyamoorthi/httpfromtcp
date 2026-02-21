be0d4161070df927df7a0193dea1bc2fb4ff196f
Prasanth Kalaiyamoorthi <prasanthkaliyamoorthi2000@gmail.com>
2026-02-21 09:17:14 +0530

    l1-tcp

--- main.py
+++ main.py
@@ -1,26 +1,36 @@
-import os
+import signal
+import socket
 
+server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_sock.bind(('127.0.0.1', 42069))
+server_sock.listen(10)
+c_conn, c_addr = server_sock.accept()
 
-def getLinesChannel(file: str):
+def sigint_handler(signal, frame):
+    print("Bye, bye!")
+    server_sock.close()
+    exit(0)
+signal.signal(signal.SIGINT, sigint_handler)
 
-    with open(file, 'r') as msg:
-        line = ""
-        while True:
-            bstr = msg.read(8)
-            if not bstr:
-                break
-            if not '\n' in bstr:
-                line += bstr
-                continue
-            parts = bstr.split('\n')
-            line += parts[0]
+def listenChannel():
+    line = ""
+    while True:
+        bstr = c_conn.recv(8).decode('utf-8')
+        if not bstr:
+            break
+        if not '\n' in bstr:
+            line += bstr
+            continue
+        parts = bstr.split('\n')
+        line += parts[0]
 
-            tmp = line
-            line = parts[1]
-            yield tmp
+        tmp = line
+        line = parts[1]
+        yield tmp
 
 def main():
-    for line in getLinesChannel('./messages.txt'):
+
+    for line in listenChannel():
         print(f"Read : {line}")
 
 
